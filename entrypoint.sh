0dd1e18b9ec02c20adde3ca8ab4ecb31091a5a6a
Qoddi App Platform <73052948+qoddiapps@users.noreply.github.com>
2024-09-23 11:37:48 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ if [ "${CUDA_ENABLED}" != "true" ]; then
     DEVICE="--device cpu"
 fi
 
-exec python tools/webui.py --llama-checkpoint-path checkpoints/fish-speech-1.4 --decoder-checkpoint-path checkpoints/fish-speech-1.4/firefly-gan-vq-fsq-8x1024-21hz-generator.pth --compile
+exec python tools/webui.py --compile
